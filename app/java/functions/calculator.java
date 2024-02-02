730be28c8ceb9963c80d309fc2b25bc926905a9e
Gellén Rebeka <91344916+redfrog66@users.noreply.github.com>
2024-02-02 15:46:31 +0000

    basic functions

--- app/java/functions/calculator.java
+++ app/java/functions/calculator.java
@@ -1,1 +1,18 @@
-function 
+public class DistanceCalculator {
+    static void steps_to_km(int steps) {
+          int currentDistance = currentDistance(); 
+          double oneStep = 1.2; /* proper value for the length of one step - future notes: adjusting this on the settings page */
+          int newDistance = steps * oneStep;
+          setNewCurrentDistance(currentDistance,newDistance);
+        
+    }
+
+    static void currentDistance() {
+        /* Csekkoljuk a linket, amit küldött Jani */
+    }
+
+    static void setNewCurrentDistance(double currentDistance, double NewDistance) {
+            double NewCurrentDistance = currentDistance + NewDistance;
+    }
+
+}
